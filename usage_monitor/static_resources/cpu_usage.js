3bc40e368c1cc47f53278fba625bda980c5dd7da
mariana-LJ <mariana.lopez.jaimez@gmail.com>
2021-01-05 20:01:07 -0800

    Displaying CPU usage in a vanilla bar chart

--- usage_monitor/static_resources/cpu_usage.js
+++ usage_monitor/static_resources/cpu_usage.js
@@ -1,5 +1,12 @@
 'use strict'
 
+const WIDTH = 800;
+const HEIGHT = 600;
+
+d3.select('svg')
+  .style('width', WIDTH)
+  .style('height', HEIGHT)
+  
 document.getElementById("update_usage").addEventListener('click', getCPUUsage);
 
 function getCPUUsage() {
@@ -7,13 +14,32 @@ function getCPUUsage() {
   .then((res) => {
     return res.json();
   })
-  .then((data) => {
-    let percentages = "";
-    console.log(data);
-    data.cpu_percent.forEach(element => {
-      percentages += element + "\t\t";
-    });
-    document.getElementById("cpu_usage").innerHTML = percentages;
+  .then((rawData) => {
+    d3.select('svg').selectAll('rect')
+    .data(rawData.cpu_percent)
+    .enter()
+    .append('rect');
+    let yScale = d3.scaleLinear();
+    yScale.range([HEIGHT, 0]);
+    let yMin = d3.min(rawData.cpu_percent);
+    let yMax = d3.max(rawData.cpu_percent);
+    yScale.domain([yMin - 1, yMax]);
+
+    d3.selectAll('rect')
+      .attr('height', (datum, index) => HEIGHT - yScale(datum));
+    
+    let xScale = d3.scaleLinear();
+    xScale.range([0, WIDTH]);
+    xScale.domain([0, rawData.cpu_percent.length]);
+    
+    d3.selectAll('rect')
+      .attr('x', (datum, index) => xScale(index));
+    
+    d3.selectAll('rect')
+      .attr('y', (datum, index) => yScale(datum));
+    
+    d3.selectAll('rect')
+      .attr('width', WIDTH/rawData.cpu_percent.length);
   })
   .catch((err) => {
     console.log(err);
